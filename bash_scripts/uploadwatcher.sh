72b1db272d7992eabcc5f6854557e069c81e9c9f
Michael Orr <michael@cloudspace.com>
2014-04-30 21:32:35 -0400

    update upload watcher shell script

--- bash_scripts/uploadwatcher.sh
+++ bash_scripts/uploadwatcher.sh
@@ -1,29 +1,33 @@
 #!/bin/bash
 
-FOUND_FILE=""
+if [[ $# -ne 1 ]]; then
+	echo "Usage: $0 </path/to/directory/to/watch>"
+	echo "You supplied $# arguments: $0 $1"
+	exit 1
+fi
+
+FILE_STARTED=""
 FILE_FINISHED="no"
 
 while :
 do
   
-  if [ -f /uploads/.file_finished_uploading ]; then
-  	echo "File Uploaded!"
+  if [ -f /$1/.file_finished_uploading ]; then
+  	echo "File Recorded as Uploaded!"
   	exit 0
   fi
-
-  if [[ $# -ne 1 ]]; then
-  	echo "Usage: $0 </path/to/directory/to/watch>"
-  	echo "You supplied $# arguments: $0 $1"
-  	exit 1
-  fi
-  
+    
   # check folder for finished file
-  if [$FILE_FOUND == ""]; then
-    FILE_FOUND=$(ls /uploads)
+  if [$FILE_STARTED == ""]; then
+  	echo "File started returned blank value: $FILE_STARTED"
+    FILE_STARTED=$(ls /$1)
+  	echo "new value: $FILE_STARTED"
   else
-    FILE_FINISHED=$(lsof|grep "/uploads/$FILE_FOUND")
+  	echo "File being Uploaded!"
+    FILE_FINISHED=$(lsof|grep "/$1/$FILE_FOUND")
     if [$FILE_FINISHED == ""]; then
-      touch /uploads/.file_finished_uploading
+      echo "File Finished Uploading!"
+      touch /$1/.file_finished_uploading
     fi
   fi
 
